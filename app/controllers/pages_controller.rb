fc795a330b87bdd0fd336d7280e45da6613627d5
Steve Brewer <steve.brewer@futurelearn.com>
2017-01-03 06:17:52 +0000

    Copy over learn page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,7 @@ class PagesController < ApplicationController
     @posts = Post.all
   end
 
+  def learn
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/learn', to: 'pages#learn', as: 'learn'
   resources :posts
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users
