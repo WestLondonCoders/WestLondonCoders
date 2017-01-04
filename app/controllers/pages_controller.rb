b02b7efca08a7d12ff2b1c71133256528bbe56e5
Steve Brewer <steve.brewer@futurelearn.com>
2017-01-04 23:48:23 +0000

    Order home page posts by most recent

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
 
   def home
     @page_title = "Home"
-    @posts = Post.all
+    @search = Post.ransack(params[:q])
+    @search.sorts = 'created_at desc' if @search.sorts.empty?
+    @posts = @search.result.includes(:created_by, :tags)
   end
 
   def learn
