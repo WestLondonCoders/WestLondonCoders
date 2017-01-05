05593d7eae9229db80fdb80bd3e2876103a535ac
Steve Brewer <steve.brewer@futurelearn.com>
2017-01-05 01:31:09 +0000

    Redirect users without a name to edit profile

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,14 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+
+    if @user.persisted?
+      sign_in(@user, event: :authentication)
+      if @user.name == ""
+        redirect_to edit_user_path(@user)
+      else
+        redirect_to root_path
+      end
+    end
   end
 end
