edb42d4aca2104d7d8b8c0367bc1806d24834379
Steve Brewer <steve.brewer@futurelearn.com>
2017-01-09 00:59:21 +0000

    Add twitter image and description settings to post editor

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :tags, through: :post_tags
   accepts_nested_attributes_for :tags, reject_if: :all_blank, allow_destroy: true
   has_many :comments, as: :commentable
+  before_save :nil_if_blank
 
   validates_presence_of :slug
 
