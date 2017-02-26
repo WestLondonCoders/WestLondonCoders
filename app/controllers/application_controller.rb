0ab34872c6928b29ac7fe8dfe9c1e07091363e81
Steve Brewer <steve.brewer@futurelearn.com>
2017-02-26 21:41:53 +0000

    Catch authorization exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to users_path, :alert => exception.message
+  end
+
   protect_from_forgery with: :exception
   after_filter :store_location
 
