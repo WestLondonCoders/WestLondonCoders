bc2269eb98bea6ce6c07507742df6cf75ce27416
Steve Brewer <steve.brewer@futurelearn.com>
2017-02-28 18:57:11 +0000

    Setup mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "people@westlondoncoders.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'people@westlondoncoders.com'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to West London Coders')
+  end
 end
