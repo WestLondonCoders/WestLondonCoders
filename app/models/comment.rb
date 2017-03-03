d8d0ae5fdce0667ff2e4bceb8d05d5c8e678c6ab
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-03 19:46:52 +0000

    Make published scope for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
   has_many :comments, as: :commentable
   belongs_to :author, class_name: "User"
   validates :body, presence: true
+
+  scope :published, -> do
+    where(public: true)
+  end
 end
