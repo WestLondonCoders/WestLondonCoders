f7a895263d2a93755ef105efb518715b59ea3baa
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-04 10:10:31 +0000

    Feature test signing in

---
+++ spec/features/signing_in.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'user sign-in' do
+
+  scenario 'user can sign in and out' do
+    as_beyonce
+    visit new_user_registration_path
+    create_an_account
+    visit user_path(@beyonce)
+    i_should_see_my_name
+  end
+
+  private
+
+  def as_beyonce
+    @beyonce = FactoryGirl.create(:user, name: 'Beyonce', email: 'beyonce@star.com', password: 'wtf123')
+  end
+
+  def create_an_account
+    fill_in 'Name', with: 'Beyonce'
+    fill_in 'Email', with: 'beyonce@star.com'
+    fill_in 'Password', with: 'wtf123'
+    click_on 'Sign up'
+  end
+
+  def i_should_see_my_name
+    expect(page).to have_content("Beyonce")
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,15 +4,16 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/poltergeist'
+ActiveRecord::Migration.maintain_test_schema!
 Capybara.javascript_driver = :poltergeist
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include Devise::Test::ControllerHelpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
