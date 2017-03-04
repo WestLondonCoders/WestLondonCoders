cd81478c8343b9da31693d698066cb2baec9375d
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-04 13:25:34 +0000

    Fix random schema.rb error

--- db/schema.rb
+++ db/schema.rb
@@ -215,7 +215,6 @@ ActiveRecord::Schema.define(version: 20170303213639) do
     t.string   "website_url"
     t.string   "linkedin"
     t.string   "tagline"
-    t.string   "slug"
     t.integer  "permission"
     t.string   "logo"
     t.string   "logo_link"
@@ -224,7 +223,6 @@ ActiveRecord::Schema.define(version: 20170303213639) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
 
   add_foreign_key "event_rsvps", "events", on_delete: :cascade
   add_foreign_key "event_rsvps", "users", on_delete: :cascade
