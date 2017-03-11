bf393ff52d3f866346109b9bf0a075cc9113efc7
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-11 23:45:28 +0000

    Fix tag relations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :posts, through: :post_tags
+  has_many :interested_users, through: :user_interests, source: :user
 end
