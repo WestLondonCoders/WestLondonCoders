3e38b8928d538e9f5560fd7b0ea05a5f3feeafc6
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-12 20:06:26 +0000

    Redesign profiles

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,4 +4,26 @@ module UserHelper
       content_tag(:i, nil, class: ["fa", "fa-#{site}"])
     end
   end
+
+  def hackroom_count(user)
+    hackrooms = user.own_hackrooms.size + user.hackrooms.size
+    pluralize(hackrooms, 'hackroom')
+  end
+
+  def interest_count(user)
+    pluralize(user.interests.size, 'interest')
+  end
+
+  def language_count(user)
+    languages = user.primary_languages.size + user.languages.size
+    pluralize(languages, 'language')
+  end
+
+  def post_count(user)
+    pluralize(user.posts.size, 'post')
+  end
+
+  def meetup_count(user)
+    pluralize(user.events.size, 'meetup')
+  end
 end
