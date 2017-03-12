4cc5a0d89579e46654fcc28aae32cd073061b49d
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-12 09:36:13 +0000

    Update view spec for home page sponsor logos

--- spec/factories/sponsor.rb
+++ spec/factories/sponsor.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     name "Net-a-porter"
     description "This sponsor has nice offices"
     link "nap.com"
+    logo { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'example-sponsor-logo.jpg')) }
+    listed true
   end
 end
--- spec/views/pages/home.html.haml_spec.rb
+++ spec/views/pages/home.html.haml_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 describe 'pages/home.html.haml' do
   let(:post) { FactoryGirl.create(:post) }
   let(:posts) { [post] }
+  let(:sponsor) { FactoryGirl.create(:sponsor) }
+  let(:sponsors) { [sponsor] }
 
   before do
     allow(posts).to receive(:take).and_return(posts)
     allow(view).to receive(:can?).and_return(false)
 
     assign(:posts, posts)
+    assign(:sponsors, sponsors)
     stub_template 'shared/_posts.html.haml' => ''
     stub_template 'pages/_comments_feed.html.haml' => ''
   end
@@ -16,6 +19,11 @@ describe 'pages/home.html.haml' do
   it 'sets page title' do
     expect(view).to receive(:set_page_title).with('Learn, discuss and practice code')
     render
+  end
+
+  it 'displays sponsor logos' do
+    render
+    expect(rendered).to have_css("img[src*='example-sponsor-logo.jpg']")
   end
 
   it 'displays featured posts' do
