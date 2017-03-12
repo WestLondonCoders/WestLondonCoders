5f3632bc30043e77946abba1ef74f41511fbd143
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-12 09:19:13 +0000

    Stop hiding notification pop up under footer

--- db/migrate/20170312074257_create_slug_for_each_language.rb
+++ db/migrate/20170312074257_create_slug_for_each_language.rb
@@ -1,6 +1,6 @@
 class CreateSlugForEachLanguage < ActiveRecord::Migration
   def change
-    Language.all.each do  |lang|
+    Language.all.each do |lang|
       slug = lang.name.downcase.strip.tr(" ", "-").tr('/', '-')
       lang.slug = slug
       lang.save
