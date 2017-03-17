d8d75429668af339eae34bf2f8e348c5f7022830
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-17 14:34:29 +0000

    Sort out layout of partner logos on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     @posts = Post.all.where(featured: true).order("created_at desc")
     @comments = Comment.all.where(public: true).order("created_at desc")
-    @sponsors = Sponsor.listed.order("name desc").take(4)
+    @sponsors = Sponsor.listed.order("name asc").take(6)
   end
 
   def learn
