f25ad272230b90615327cfe899c4ad5a05fe3a74
Steve Brewer <steve.v.brewer@gmail.com>
2017-06-04 17:05:14 +0100

    Show our event RSVP button, not meetup.com

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
     @posts = Post.all.where(featured: true).order("created_at desc")
     @comments = Comment.all.where(public: true).order("created_at desc")
     @sponsors = Sponsor.listed.order("name asc").take(6)
+    @next_event = Event.upcoming.first
   end
 
   def learn
