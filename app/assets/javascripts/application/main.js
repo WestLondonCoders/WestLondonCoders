89348216f9642ec13525bda8eb2ec12ea8fecd20
Steve Brewer <steve.v.brewer@gmail.com>
2017-06-04 17:12:58 +0100

    Stop sound

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,6 +1,4 @@
 $(document).on('turbolinks:load', function() {
   $('.chosen-select').chosen({ allow_single_deselect: true, width: '200px' });
   $('.colour-select').minicolors();
-  var videoHero = document.getElementById('home-vid');
-  videoHero.muted = "muted";
 });
