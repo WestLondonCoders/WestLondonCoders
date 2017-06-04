d08fb718798b0f0a75022c99bdbc289c2800557f
Steve Brewer <steve.v.brewer@gmail.com>
2017-06-04 19:52:41 +0100

    Steve follows everyone

---
+++ db/migrate/20170604184431_steve_follows_everyone.rb
@@ -0,0 +1,13 @@
+class SteveFollowsEveryone < ActiveRecord::Migration
+  def change
+    User.all.each do |u|
+      record = UserFollow.find_by(follower: User.find(1), user: u)
+      unless record
+        steve = User.find(1)
+        UserFollow.create(follower: steve, user: u)
+        u.user_follows_count += 1
+        Notification.create(user: u, notified_by: steve, notifiable: u, action: 'followed')
+      end
+    end
+  end
+end
