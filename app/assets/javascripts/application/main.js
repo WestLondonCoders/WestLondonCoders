166e54242d0895baa58816342649751946cfe4bf
Steve Brewer <steve.v.brewer@gmail.com>
2017-06-04 17:05:26 +0100

    Mute video with JS, not Rails

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $('.chosen-select').chosen({ allow_single_deselect: true, width: '200px' });
   $('.colour-select').minicolors();
+  var videoHero = document.getElementById('home-vid');
+  videoHero.muted = "muted";
 });
