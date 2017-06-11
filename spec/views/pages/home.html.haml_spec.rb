5a1b43120167cccf8b22ccbab6b9b97e68d8adc3
Steve Brewer <steve.v.brewer@gmail.com>
2017-06-11 22:32:03 +0100

    Add RSVP button view specs

--- spec/views/pages/home.html.haml_spec.rb
+++ spec/views/pages/home.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe 'pages/home.html.haml' do
   let(:posts) { [post] }
   let(:sponsor) { FactoryGirl.create(:sponsor) }
   let(:sponsors) { [sponsor] }
-  let(:next_meetup) { double(:meetup, date: 1.week.from_now, finish_date: 1.week.from_now) }
+  let(:next_meetup) { FactoryGirl.create(:meetup, date: 1.week.from_now, finish_date: 1.week.from_now) }
 
   before do
     allow(posts).to receive(:take).and_return(posts)
@@ -36,5 +36,29 @@ describe 'pages/home.html.haml' do
   it 'displays latest comments' do
     render
     expect(rendered).to render_template(partial: '_comments_feed')
+  end
+
+  it 'displays the rsvp button for the next event' do
+    render
+    expect(rendered).to have_link('RSVP', href: new_user_session_path)
+  end
+
+  context 'when signed in' do
+    let(:current_user) { FactoryGirl.create(:user) }
+
+    before do
+      allow(view).to receive(:current_user).and_return(current_user)
+    end
+
+    it 'displays the rsvp button for the next meetup' do
+      render
+      expect(rendered).to have_link('RSVP', href: rsvp_meetup_path(next_meetup))
+    end
+
+    it 'displays the cancel rsvp button if I am attending' do
+      allow(view).to receive(:user_attending_meetup?).and_return(true)
+      render
+      expect(rendered).to have_link('Cancel your RSVP', href: rsvp_meetup_path(next_meetup))
+    end
   end
 end
