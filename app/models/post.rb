86e7da66de3174c6ed91d8b6f797e3d0ef7e39b8
Steve Brewer <steve.brewer@futurelearn.com>
2017-06-22 00:35:46 +0100

    Remove post attachment associations from post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :created_by, class_name: "User"
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
-  has_many :post_attachments
   has_many :comments, as: :commentable, dependent: :destroy
 
   accepts_nested_attributes_for :tags, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :post_attachments, reject_if: :all_blank, allow_destroy: true
 
   validates_presence_of :slug
 
