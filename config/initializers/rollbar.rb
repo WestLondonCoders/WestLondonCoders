ee6d12a77161180511f794c65ee80e2940a2647b
Steve Brewer <steve.brewer@futurelearn.com>
2017-06-24 00:43:39 +0100

    Only log to Rollbar in production

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,6 +2,7 @@ require 'rollbar'
 
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_TOKEN']
+  unless Rails.env.production?
+    config.enabled = false
+  end
 end
-
-# Rollbar.error('Hi Steve')
