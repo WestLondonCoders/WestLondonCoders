d10b1ad71726d2356fc9c465088b438509a51d13
Steve Brewer <steve.brewer@futurelearn.com>
2017-06-24 12:32:35 +0100

    Stop posting to Slack in tests

--- app/models/meetup_rsvp.rb
+++ app/models/meetup_rsvp.rb
@@ -1,6 +1,6 @@
 class MeetupRsvp < ActiveRecord::Base
-  after_create :announce_rsvp
-  after_destroy :announce_rsvp_cancelled
+  after_create :announce_rsvp if  Rails.env.production?
+  after_destroy :announce_rsvp_cancelled if  Rails.env.production?
 
   belongs_to :meetup
   belongs_to :user
@@ -9,19 +9,15 @@ class MeetupRsvp < ActiveRecord::Base
 
   def announce_rsvp
     message = "#{user.name} is now attending the meetup on #{meetup.start_date} at #{meetup.sponsor.name} #{meetup_path}"
-    Slacked.post_async message, channel: slack_channel, username: 'Hackroom Bot'
+    Slacked.post_async message, channel: 'rsvp-notifications', username: 'Hackroom Bot'
   end
 
   def announce_rsvp_cancelled
     message = "#{user.name} can no longer attend the meetup on #{meetup.start_date} at #{meetup.sponsor.name} #{meetup_path}"
-    Slacked.post_async message, channel: slack_channel, username: 'Hackroom Bot'
+    Slacked.post_async message, channel: 'rsvp-notifications', username: 'Hackroom Bot'
   end
 
   def meetup_path
     "http://westlondoncoders.com" + Rails.application.routes.url_helpers.meetup_path(meetup)
-  end
-
-  def slack_channel
-    Rails.env.production? ? 'rsvp-notifications' : 'testing'
   end
 end
