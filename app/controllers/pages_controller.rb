0bf7cf24ac3ee0f5ee9d4265bb3a873498af9574
Steve Brewer <steve.brewer@futurelearn.com>
2017-06-24 12:27:04 +0100

    Remove old routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,8 @@
 class PagesController < ApplicationController
-
   def home
     @posts = Post.all.where(featured: true).order("created_at desc")
     @comments = Comment.top_level.published.most_recent_first.take(6)
     @sponsors = Sponsor.listed.order("name asc").take(6)
     @next_meetup = Meetup.in_upcoming_order.first
   end
-
-  def learn
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/learn', to: 'pages#learn', as: 'learn'
-  get '/jekyll', to: 'pages#jekyll', as: 'jekyll'
   get '/organisers', to: 'users#organisers', as: 'organisers'
   get '/past-meetups', to: 'meetups#past_meetups', as: 'past_meetups'
   get 'events', to: redirect('meetups')
