99fc70acf52ebfa1f15f0a23b5bf55589e2d9c10
Steve Brewer <steve.brewer@futurelearn.com>
2017-10-06 21:38:33 +0100

    Fix after_sign_in URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def store_location
-    session[:previous_url] = request.fullpath if request.fullpath.match? %r{users/}
+    session[:previous_url] = request.fullpath unless request.fullpath.match? %r{users/}
   end
 
   def after_sign_in_path_for(resource)
